fa45c508fe45721cc14529e1809d1952a9b026bc
ishayasolo <ishaya808@gmail.com>
2024-02-25 01:47:56 +0100

    Update the three state tiles (start, flipped and matched) to suit the designs

--- src/Tile.jsx
+++ src/Tile.jsx
@@ -3,32 +3,32 @@ export function Tile({ content: Content, flip, state }) {
     case "start":
       return (
         <Back
-          className="inline-block h-8 w-8 bg-blue-300 text-center"
+          className="inline-block h-auto aspect-square w-full bg-blue-300 rounded-lg"
           flip={flip}
         />
       );
     case "flipped":
       return (
-        <Front className="inline-block h-8 w-8 bg-green-500">
+        <Front className="inline-block h-auto aspect-square w-full bg-blue-500 text-white px-2 rounded-lg">
           <Content
             style={{
               display: "inline-block",
               width: "100%",
               height: "100%",
-              verticalAlign: "top",
+              verticalAlign: "middle",
             }}
           />
         </Front>
       );
     case "matched":
       return (
-        <Matched className="inline-block h-8 w-8 text-gray-300">
+        <Matched className="inline-block h-auto aspect-square w-full bg-trnsparent text-blue-200 px-2 rounded-lg">
           <Content
             style={{
               display: "inline-block",
               width: "100%",
               height: "100%",
-              verticalAlign: "top",
+              verticalAlign: "middle",
             }}
           />
         </Matched>
@@ -39,11 +39,7 @@ export function Tile({ content: Content, flip, state }) {
 }
 
 function Back({ className, flip }) {
-  return (
-    <div onClick={flip} className={className}>
-      ?
-    </div>
-  );
+  return <div onClick={flip} className={className}></div>;
 }
 
 function Front({ className, children }) {
